f4ea30e5827dfefbeb4c887269054cc460882850
ravindra-singh94 <ravindrarajpoot9628172@gmail.com>
2020-01-19 17:20:27 +0530

    netflex webpage added

---
+++ netflex/index.js
@@ -0,0 +1,23 @@
+const tabItems= document.querySelectorAll(".tab-item");
+const tabContentItems= document.querySelectorAll(".tab-content-item");
+//select tab content item 
+function selectItem(e){
+    removeBorder();
+    removeShow();
+    this.classList.add('tab-border');
+    //grab content item from dom
+    console.log(this.id);
+    const tabContentItems= document.querySelector(`#${this.id}-content`);
+    console.log(tabContentItems);
+    tabContentItems.classList.add('show');
+
+}
+function removeBorder(){
+    tabItems.forEach(item => item.classList.remove('tab-border'))
+}
+function removeShow(){
+    tabContentItems.forEach(item => item.classList.remove('show'));
+}
+
+//listen for content item
+tabItems.forEach(item => item.addEventListener('click', selectItem)) 
