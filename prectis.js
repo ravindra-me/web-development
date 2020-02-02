79c34f3aacbed01e675e8827a8cfdd1919961663
ravindra-singh94 <ravindrarajpoot9628172@gmail.com>
2020-02-02 18:55:48 +0530

    advanced array

--- prectis.js
+++ prectis.js
@@ -10,3 +10,14 @@ const mapArray = array.map((num) => {
     return num*2;
 });
 console.log('map', mapArray);
+// filter method
+const filterArray = array.filter((num) =>{
+    return num >5;
+} );
+console.log('filter' , filterArray);
+
+//reduce
+const reduceArray = array.reduce((accumulator , num)=>{
+    return accumulator+num;
+})
+console.log(reduceArray);
